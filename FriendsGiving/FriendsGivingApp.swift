3dd354e36abfbe1280922140640eada0d4bbde74
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-28 09:31:18 -0400

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Francesca MACDONALD on 2023-08-28.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
